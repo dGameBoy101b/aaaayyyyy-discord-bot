e8e14ed75069873c62561ec4370cd104aea3ca2c
dGameBoy101b <rhysmader@gmail.com>
2022-02-10 13:26:20 +0800

    Fix Logging
    Removed all unnecessary logger is none checks.
    Fixed a logging statement using the wrong logger.
    Expanded all usernames and also include the discriminator.

--- aaaayyyyy_bot.py
+++ aaaayyyyy_bot.py
@@ -35,16 +35,14 @@ or set of existing keys if given a guild or member'''
         '''Trigger a ping with the given message'''
         #calc target list key
         key = self.get_target_list_key(message.channel)
-        if self.logger is not None:
-            self.logger.debug(f'Fetching target list for {key}')
+        self.logger.debug(f'Fetching target list for {key}')
         #calc target list
         targets = set()
         if key in self.targets:
             targets = self.targets[key] & set(map(lambda mem: mem.mention, message.channel.members))
         #log action
-        if logger is not None:
-            logger.info('Pinging "' + '", "'.join(targets)
-                        + f'" in channel "{message.channel}" in guild "{message.guild}"')
+        self.logger.info('Pinging "' + '", "'.join(map(lambda mention: mention, targets))
+                    + f'" in channel "{message.channel}" in guild "{message.guild}"')
         #ping targets
         await message.channel.send(f'Aaaayyyyy! {" ".join(targets)}', delete_after=self.ping_timeout,
                                    reference=message, mention_author=False)
@@ -53,12 +51,10 @@ or set of existing keys if given a guild or member'''
         '''Add or remove targets mentioned in the given message'''
         #get key
         key = self.get_target_list_key(message.channel)
-        if self.logger is not None:
-            self.logger.debug(f'Modifying target list for {key}')
+        self.logger.debug(f'Modifying target list for {key}')
         if key not in self.targets:
             self.targets[key] = set()
-            if self.logger is not None:
-                self.logger.debug(f'Added new target list for {key}')
+            self.logger.debug(f'Added new target list for {key}')
         #classify targets
         to_remove = set()
         to_add = set()
@@ -77,71 +73,66 @@ or set of existing keys if given a guild or member'''
             if self.logger is not None:
                 self.logger.debug(f'Removed empty target list from {key}')
         #log actions
-        if self.logger is not None:
-            if len(to_remove) > 0:
-                self.logger.info(f'Removed "' + '", "'.join(map(lambda user: user.name, to_remove))
-                                 + f'" from target list for channel "{message.channel}" in guild "{message.guild}"')
-            if len(to_add) > 0:
-                self.logger.info(f'Added "' + '", "'.join(map(lambda user: user.name, to_add))
-                                 + f'" to target list for channel "{message.channel}" in guild "{message.guild}"')
+        if len(to_remove) > 0:
+            self.logger.info(f'Removed "' + '", "'.join(map(lambda user: f'{user.name}#{user.discriminator}', to_remove))
+                             + f'" from target list for channel "{message.channel}" in guild "{message.guild}"')
+        if len(to_add) > 0:
+            self.logger.info(f'Added "' + '", "'.join(map(lambda user: f'{user.name}#{user.discriminator}', to_add))
+                             + f'" to target list for channel "{message.channel}" in guild "{message.guild}"')
         #reply to command
         reply = list()
         if len(to_remove) > 0:
-            reply.append('Removed `' + '`, `'.join(map(lambda user: user.name, to_remove))
+            reply.append('Removed `' + '`, `'.join(map(lambda user: f'{user.name}#{user.discriminator}', to_remove))
                          + '` from target list')
         if len(to_add) > 0:
-            reply.append('Added `' + '`, `'.join(map(lambda user: user.name, to_add))
+            reply.append('Added `' + '`, `'.join(map(lambda user: f'{user.name}#{user.discriminator}', to_add))
                          + '` to target list')
         await message.channel.send('\n'.join(reply), delete_after=self.command_timeout,
                                    reference=message, mention_author=False)
         return
     async def on_message(self, message):
-        if message.author != self.user:
-            if self.user in message.mentions:
-                await self.command(message)
-            elif 'a' in message.content or 'A' in message.content:
-                await self.ping(message)
+        if message.author == self.user:
+            return
+        if self.user in message.mentions:
+            await self.command(message)
+        elif 'a' in message.content or 'A' in message.content:
+            await self.ping(message)
         return
     async def on_guild_channel_delete(self, channel):
         key = self.get_target_list_key(channel)
         if key in self.targets:
             del self.targets[key]
-            if self.logger is not None:
-                self.logger.info(f'Cleared targets for channel "{channel.name}" in guild "{channel.guild.name}"')
+            self.logger.info(f'Cleared targets for channel "{channel.name}" in guild "{channel.guild.name}"')
         return
     async def on_guild_remove(self, guild):
         keys = self.get_target_list_key(guild)
         if len(keys) > 0:
             for key in keys:
                 del self.targets[key]
-            if self.logger is not None:
-                self.logger.info('Cleared targets for channels "'
-                                 + '", "'.join(map(lambda key: guild.get_channel(key[1]).name, keys))
-                                 + f'" in guild "{guild.name}"')
+            self.logger.info('Cleared targets for channels "'
+                             + '", "'.join(map(lambda key: guild.get_channel(key[1]).name, keys))
+                             + f'" in guild "{guild.name}"')
         return
     async def on_private_channel_delete(self, channel):
         key = self.get_target_list_key(channel)
         if key in self.targets:
             del self.targets[key]
-            if self.logger is not None:
-                self.logger.info(f'Cleared targets for channel "{channel.name}"')
+            self.logger.info(f'Cleared targets for channel "{channel.name}"')
         return
     async def on_member_remove(self, member):
         keys = self.get_target_list_key(member)
         if len(keys) > 0:
             for key in keys:
                 self.targets[key].remove(member.mention)
-            if self.logger is not None:
-                self.logger.info(f'Removed target "{member.name}" from channels "'
-                                 + '", "'.join(map(lambda key: member.guild.get_channel(key[1]).name, keys))
-                                 + f'" in guild "{member.guild.name}"')
+            self.logger.info(f'Removed target "{member.name}#{member.discriminator}" from channels "'
+                             + '", "'.join(map(lambda key: member.guild.get_channel(key[1]).name, keys))
+                             + f'" in guild "{member.guild.name}"')
         return
     async def on_group_remove(self, channel, user):
         key = self.get_target_list_key(channel)
         if key in self.targets and user.mention in self.targets[key]:
             self.targets[key].remove(user.mention)
-            if self.logger is not None:
-                self.logger.info(f'Removed target "{user.name}" from group channel "{channel.name}"')
+            self.logger.info(f'Removed target "{user.name}#{user.discriminator}" from group channel "{channel.name}"')
         return
             
 if __name__ == '__main__':
